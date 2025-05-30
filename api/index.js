f1e49d2e72fcdc148f5ad7ea9bb21e8344701022
himesh220002 <satyamhimesh@gmail.com>
2025-05-30 13:38:22 +0530

    a

--- api/index.js
+++ api/index.js
@@ -1,7 +1,7 @@
 // api/index.js
 const express = require("express");
 const serverless = require("serverless-http");
-const app = require("../../app"); // assuming `app.js` is at root level
+const app = require("../app"); // assuming `app.js` is at root level
 
 module.exports = app;
 module.exports.handler = serverless(app);
