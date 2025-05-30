9ca897252b01bca95dcf401a5a468a9a10203b2d
himesh220002 <satyamhimesh@gmail.com>
2025-05-30 23:51:47 +0530

    u

--- app.js
+++ app.js
@@ -115,7 +115,7 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: process.env.GOOGLE_CALLBACK_URL || "http://localhost:3000/auth/google/secrets"
+      callbackURL: process.env.GOOGLE_CALLBACK_URL 
 
     },
     function (accessToken, refreshToken, profile, cb) {
