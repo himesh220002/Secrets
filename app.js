838d66a4010ea3c21ae35298497c14783d7f7ea9
himesh220002 <satyamhimesh@gmail.com>
2025-05-31 15:03:21 +0530

    t

--- app.js
+++ app.js
@@ -275,7 +275,7 @@ app.get("/mypost", isAuthenticated, async (req, res) => {
   const mySecrets = (user.secret || []).map((item) => ({
     _id: item._id,
     text: item.text,
-    createdAt: item.createdAt
+    createdAt: moment(secret.createdAt).tz("Asia/Kolkata").format("YYYY-MM-DD HH:mm:ss")
   }));
 
   res.render("mypost", {
